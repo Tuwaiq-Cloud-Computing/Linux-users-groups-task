9fb0f80eb0bd9733f5bf6ebfe1a385fb20df41a3
abdullahAbusabba <alajmi7abdullah@gmail.com>
2023-01-18 19:49:46 +0300

    adding users and groups

---
+++ index.py
@@ -0,0 +1,40 @@
+import os
+import crypt
+
+
+encPass= crypt.crypt("alibaba@s1","22")
+ExpirationDate="2025-01-01"
+user_groups = [
+	{
+		"groupName":"CEO",
+		"users":["Ali"]
+	},
+	{
+		"groupName":"Manager",
+		"users":["Salem"]
+	},
+	{
+		"groupName":"HR",
+		"users":["Salem","Norah","Deem"]
+	},
+	{
+		"groupName":"Shipping",
+		"users":["Sara","Naif"]
+	},
+	{
+		"groupName":"Sales",
+		"users":["Hanan","Khaled"]
+	}
+
+]
+
+
+for group in user_groups:
+	os.system(f'groupadd {group["groupName"]}')
+	for user in group["users"]:
+		os.system(f'useradd {user} --gid {group["groupName"]} --groups {group["groupName"]} --password {encPass} --expiredate {ExpirationDate}')
+		
+
+
+
+
